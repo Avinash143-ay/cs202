709eefdc6868658faeccfecbbe7bb632a377fe5d
avinash reddy <143334209+Avinash143-ay@users.noreply.github.com>
2025-08-04 15:21:25 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -3,9 +3,10 @@ def is_prime(n):
         return False
     for i in range(2, int(n**0.5)+1):
         if n % i == 0:
-            return False
+            return 2False
     return True
 
 for i in range(1, 50):
     print(f"{i} is prime: {is_prime(i)}")
 #prime number
+#another
